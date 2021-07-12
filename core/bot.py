da544354518f4225cf4d4e43b8237b7c7b1971c6
Monczak <szymon.jedrzejczak.sj@gmail.com>
2021-07-12 14:13:54 +0200

    Add config.json

--- core/bot.py
+++ core/bot.py
@@ -1,14 +1,32 @@
+import os
+import json
 from discord.ext import commands
 from core.logger import Logger
 
 from core.commands.test_commands import TestCommands
 
-client = commands.Bot(command_prefix="n!")
+
+def load_config():
+    if os.path.exists("config/config.json"):
+        with open("config/config.json", "r") as file:
+            config_json = file.read()
+        config = json.loads(config_json)
+        return config
+    else:
+        Logger.error("No config file found")
+        exit(1)
+
+
+config = load_config()
+
+Logger.log(f"Prefix is {config['commandPrefix']}")
+
+client = commands.Bot(command_prefix=config["commandPrefix"])
 
 
 @client.event
 async def on_ready():
-    Logger.log(f"Logged in as {client}")
+    Logger.log(f"Logged in as {client.user.display_name}#{client.user.discriminator}")
 
 
 def start_bot(token):
